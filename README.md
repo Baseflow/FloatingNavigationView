5dbbfef30a6f66229273f38b7c2e55469dce533b
Martijn van Dijk <mhvdijk@gmail.com>
2016-09-30 14:54:02 +0200

    Add initial plugin

---
+++ README.md
@@ -0,0 +1,10 @@
+# FloatingNavigationView
+
+Xamarin bindings library for Floating-Navigation-View [library](https://github.com/andremion/Floating-Navigation-View).
+
+Plugin is available on [Nuget](https://www.nuget.org/packages/Xam.Plugins.Android.FloatingNavigationView/).
+
+#### Preview
+![Sample](https://raw.githubusercontent.com/andremion/Floating-Navigation-View/master/art/sample.gif)
+
+See the sample for implementation.
---
+++ Sample/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Com.Andremion.Floatingnavigationview;
+
+namespace Sample
+{
+    [Activity(Label = "Sample", MainLauncher = true, Icon = "@mipmap/icon", Theme="@style/AppTheme.NoActionBar")]
+    public class MainActivity : AppCompatActivity, View.IOnClickListener, NavigationView.IOnNavigationItemSelectedListener
+    {
+        private FloatingNavigationView mFloatingNavigationView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            mFloatingNavigationView = FindViewById<FloatingNavigationView>(Resource.Id.floating_navigation_view);
+
+            mFloatingNavigationView.SetOnClickListener(this);
+            mFloatingNavigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            Snackbar.Make((View)mFloatingNavigationView.Parent, menuItem.TitleFormatted.ToString() + " Selected!", Snackbar.LengthShort).Show();
+            mFloatingNavigationView.Close();
+            return true;
+        }
+
+        public void OnClick(View v)
+        {
+            mFloatingNavigationView.Open();
+        }
+
+        public override void OnBackPressed()
+        {
+            if (mFloatingNavigationView.IsOpened)
+            {
+                mFloatingNavigationView.Close();
+            }
+            else {
+                base.OnBackPressed();
+            }
+        }
+    }
+}
+
